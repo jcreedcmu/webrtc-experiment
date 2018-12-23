e0c064e59ff21d1e8f1161d5fad0eadd365c95d0
Jason Reed <jcreed@gmail.com>
2018-12-23 11:42:11 -0500

    combine stages

--- src/alice.ts
+++ src/alice.ts
@@ -2,6 +2,7 @@ type Global = {
   peer?: RTCPeerConnection,
   channel?: RTCDataChannel,
 }
+
 type Invite = {
   cand?: RTCIceCandidate,
   offer?: RTCSessionDescriptionInit,
@@ -13,7 +14,7 @@ type Cmd =
   | { t: 'added', id: string }
   | { t: 'response', payload: RTCSessionDescriptionInit };
 
-class Alice {
+class Principal {
   invite: Invite = {};
   glob: Global = {};
   proto = location.protocol.replace(/http/g, 'ws');
@@ -95,6 +96,56 @@ class Alice {
     }
   }
 
+  bobStage1(invite: Invite, id: string) {
+    const { glob, proto } = this;
+    const { offer, cand } = invite;
+    if (offer == undefined || cand == undefined) {
+      throw "malformed invite";
+    }
+
+    const peer = glob.peer = new RTCPeerConnection({
+      iceServers: [
+        { urls: ['stun:stun.l.google.com:19302'] }]
+    });
+
+    peer.ondatachannel = (ch) => {
+      const channel = ch.channel;
+      glob.channel = channel;
+      channel.onopen = () => console.log('open');
+      channel.onclose = () => console.log('close');
+      channel.onerror = (e) => console.log('error', e);
+      channel.onmessage = (e) => addLine(e.data);
+    }
+
+    peer.onicecandidate = function(event) {
+      if (event.candidate) {
+        console.log('ICE CAND!', event.candidate);
+        if (localStorage.bob == undefined) {
+          localStorage.bob = JSON.stringify(event.candidate);
+        }
+      }
+    };
+
+    peer.setRemoteDescription(new RTCSessionDescription(offer))
+      .then(() => { console.log('succ'); })
+      .catch(carp('setRemoteDescription'));
+
+    peer.createAnswer()
+      .then((answer) => {
+        console.log('answered');
+        const ws = new WebSocket(proto + "//" + location.host + "/ws");
+        ws.onopen = () => {
+          ws.send(JSON.stringify({ t: "respond", id, payload: answer }));
+        };
+        peer.setLocalDescription(answer);
+      })
+      .catch(carp('createAnswer'));
+
+    peer.addIceCandidate(cand)
+      .then(() => console.log("ice succ"))
+      .catch((e) => console.error("ice fail", e));
+  }
+
   chatLine(e: Event) {
     const { glob } = this;
     e.stopPropagation();
@@ -112,7 +163,3 @@ class Alice {
 function addLine(s: string) {
   ($("#chat")[0] as HTMLInputElement).value += "\n" + s;
 }
-
-const g = window as any;
-g.Alice = Alice;
-export = {};
