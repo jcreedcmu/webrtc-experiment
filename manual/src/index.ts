e899727774f9686bebb5bd967305089d7e6bc509
Jason Reed <jcreed@gmail.com>
2022-10-07 16:15:54 -0400

    further refactoring

--- manual/src/index.ts
+++ manual/src/index.ts
@@ -44,13 +44,12 @@ async function getOffer(peer: RTCPeerConnection): Promise<Tokens> {
   return { cand, sessionInit: offer };
 }
 
-async function useOffer(tokens: Tokens): Promise<Bundle> {
-  const peer = createPeer();
+async function useOffer(peer: RTCPeerConnection, tokens: Tokens): Promise<Tokens> {
   await peer.setRemoteDescription(new RTCSessionDescription(tokens.sessionInit));
   const answer = await peer.createAnswer();
   peer.setLocalDescription(answer);
   const cand = await getFirstIceCandidate(peer);
-  return { peer, tokens: { cand, sessionInit: answer } };
+  return { cand, sessionInit: answer };
 }
 
 async function useAnswer(bundle: Bundle): Promise<void> {
@@ -59,40 +58,48 @@ async function useAnswer(bundle: Bundle): Promise<void> {
   await peer.addIceCandidate(tokens.cand);
 }
 
-function broadcastOffer(id: string, offer: Tokens): Promise<Tokens> {
-  console.log(`Run this in another window:
-step2(${JSON.stringify(offer)})`);
+function fakeWrite(id: string, offer: Tokens): void {
+  console.log(`You should call
+transmit(${JSON.stringify(id)}, ${JSON.stringify(offer)});
+in the other window`);
+}
+
+const table: Record<string, (data: Tokens) => void> = {};
+
+function fakeRead(id: string): Promise<Tokens> {
   return new Promise((res, rej) => {
-    G.step3 = (answer: Tokens) => res(answer);
+    table[id] = res;
   });
 }
 
-function signalAnswer(id: string, answer: Tokens): void {
-  console.log(`Run this in original window:
-step3(${JSON.stringify(answer)})`);
+function transmit(id: string, data: Tokens): void {
+  table[id](data);
 }
 
 /////
 
 const G: any = window;
+G.transmit = transmit;
 
-G.step1 = () => {
+G.hlListen = () => {
   (async () => {
     const peer = createPeer();
     const channel = createChannel(peer);
     const offer = await getOffer(peer);
-    const answerPromise = broadcastOffer('alice', offer);
-    const answer = await answerPromise;
+    await fakeWrite('alice.offer', offer);
+    const answer = await fakeRead('alice.answer');
     useAnswer({ peer, tokens: answer });
     console.log('opened data channel!');
     G.channel = channel;
   })();
 }
 
-G.step2 = (invite: Tokens) => {
+G.hlConnect = (invite: Tokens) => {
   (async () => {
-    const { peer, tokens: answer } = await useOffer(invite);
-    signalAnswer('alice', answer);
+    const peer = createPeer();
+    const offer = await fakeRead('alice.offer');
+    const answer = await useOffer(peer, offer);
+    await fakeWrite('alice.answer', answer);
     const channel = await getDataChannel(peer);
     initDataChannel(channel);
     console.log('opened data channel!');
